e8de113243ca423a029c5f8efc17c54f96a342c9
Victor Henrique da Silva Froes Dantas <henrique24.dantas@gmail.com>
2025-03-31 20:38:04 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while(c<=6) {
+    console.log('Tudo bem?')
+    c++ /* c + 1 */
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
